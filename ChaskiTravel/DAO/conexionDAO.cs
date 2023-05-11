312ae44baae02ddf9d8e95064594a2d9788a0f0f
vrquim <vrquim@outlook.com>
2023-05-11 16:06:54 -0500

    Mejorando inicio y contacto

--- ChaskiTravel/DAO/conexionDAO.cs
+++ ChaskiTravel/DAO/conexionDAO.cs
@@ -7,7 +7,7 @@ namespace ChaskiTravel.DAO
         //definicion de la conexion ( Cambien el server DESKTOP-OE6VUV5, por el de ustedes 
         //para que funcione la conexion a la bd)
         SqlConnection cn = new SqlConnection(
-        @"server = JDHQ\SQLEXPRESS;database = BDAtipax;Trusted_Connection = True;" +
+        @"server = DESKTOP-2K8V4T5;database = BDChaskiTravel;Trusted_Connection = True;" +
          "MultipleActiveResultSets = True;TrustServerCertificate = False;Encrypt = False");
 
         //lectura de la conexion, get
