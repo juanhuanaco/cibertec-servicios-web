e832b78ea23c13953c4e2a91d346b241cf726757
Herber Villegas <herber.villegas.hu@gmail.com>
2023-05-25 21:27:04 -0500

    firts commit Herber

--- ChaskiTravel/DAO/conexionDAO.cs
+++ ChaskiTravel/DAO/conexionDAO.cs
@@ -7,7 +7,7 @@ namespace ChaskiTravel.DAO
         //definicion de la conexion ( Cambien el server DESKTOP-OE6VUV5, por el de ustedes 
         //para que funcione la conexion a la bd)
         SqlConnection cn = new SqlConnection(
-        @"server = JDHQ\SQLEXPRESS;database = BDAtipax;Trusted_Connection = True;" +
+        @"server = DESKTOP-T1FV3HT\SQLEXPRESS;database = BDAtipax;Trusted_Connection = True;" +
          "MultipleActiveResultSets = True;TrustServerCertificate = False;Encrypt = False");
 
         //lectura de la conexion, get
